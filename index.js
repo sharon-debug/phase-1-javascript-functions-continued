f5d62f0c0d715b8ddd4d2aa7e2ed5c869f647cdc
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-08-20 12:21:48 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+//uses the default activity 'roller-skate' when no arguments are passed
+function saturdayFun(activity = 'roller-skate'){
+    return(`This Saturday, I want to ${activity}!`);
+}
+//permits the default activity to be overriden
+saturdayFun('bathe my dog');
+
+//uses the default activity 'go to the office' when no arguments are passed
+
+const mondayWork = function(activity = 'go to the office'){
+    return(`This Monday, I will ${activity}.`);
+}
+//permits the default activity to be overriden
+mondayWork('work from home');
+
+//when initialized with '*' creates a function that, when called, wraps an adjective in a highlight
+
+function wrapAdjective(visual = '*'){
+    return function(adj = "special"){
+        return `You are ${visual}${adj}${visual}!`
+    }
+}
